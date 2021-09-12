f132c5dc8c56443a66fee0b0335ee2c7c5aec0f2
ajins15 <ansengarie@gmail.com>
2021-09-12 14:51:08 +0700

    add assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun,
-  saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/profile.png" alt="Aji Nuansa Sengarie" width="300" class="rounded rounded-circle mx-auto d-block">
+  <p class="text-center mt-3">Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun,
+    saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,17 @@
-<h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+  <div class="jumbotron m-3">
+    <h1 class="display-4">
+      Selamat Datang di Website Saya!
+    </h1>
+    <p class="lead">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque eaque adipisci, aliquid sed quisquam eveniet voluptate.
+    </p>
+    <hr class="my-4">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, quae?
+    </p>
+    <a href="" class="btn btn-primary btn-lg" role="button">
+      Learn More
+    </a>
+  </div>
+</div>
